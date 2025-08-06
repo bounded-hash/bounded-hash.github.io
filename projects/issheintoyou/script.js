06dfc48f7197a9ea0d3182704a69cab799bae288
bounded-hash <thisisotajonov@gmail.com>
2025-08-06 15:08:19 +0500

    add projects page

--- projects/issheintoyou/script.js
+++ projects/issheintoyou/script.js
@@ -17,7 +17,7 @@ function answer(button){
 		qIndex += 1;
 	} else {
 		quiz.innerHTML += '<a href="https://www.youtube.com/watch?v=xa-4IAR_9Yw&ab_channel=CasuallyExplained"> context </a>'
-		buttons.innerHTML = '<a href="./issheintoyou.html"> return </a>'
+		buttons.innerHTML = '<a href="/"> return </a>'
 	}
 }
 
@@ -25,7 +25,7 @@ function skip(){
 	const txt = quizData[qValues[qIndex]]
 	quiz.innerHTML = txt;
 	buttons.innerHTML = '<button class="wrong" onclick="answer(this)"> yes </button> \
-				<button class="wrong" onclick="answer(this)"> no </button> \
+				<button class="maybe" onclick="answer(this)"> no </button> \
 				<button class="correct" onclick="answer(this)"> can\'t tell </button>'
 	qIndex += 1;
 }
